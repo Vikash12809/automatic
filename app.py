2ee249a74483c80b8abaeef5e268b98fb0bd037b
Vikash12809 <82423320+Vikash12809@users.noreply.github.com>
2021-04-14 16:27:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def getHandler():
      PA = r["address"]
      PS = r["amount"]
      PR = r["tokenid"]
-     txn = tron.trx.send_token(PA, 10*100000*PS, PR);
+     txn = tron.trx.send_token(PA, 1*PS, PR););
      return txn["transaction"]["txID"]
     
     
